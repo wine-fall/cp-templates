48bedc990abb603d0948c05954fcb7f84d405c72
liruiyuan <384650157@qq.com>
2023-01-07 16:34:25 +0800

    feat: 拓扑排序

--- graph/find_cycle.py
+++ graph/find_cycle.py
@@ -1,4 +1,5 @@
 # DFS找环
+# leetcode题目: 207-课程表，802-找到最终的安全状态
 def dfs(x):
     if flags[x] > 0:
         return flags[x] == 2
---
+++ graph/topo_sort.py
@@ -0,0 +1,24 @@
+# 拓扑排序
+# 参考资料: https://oi-wiki.org/graph/topo/
+# leetcode题目: 210-课程表 II
+adj = defaultdict(list)
+for y, x in graph:
+    adj[x].append(y)
+
+flags = [0] * n
+ret = []
+
+def dfs(x):
+    flags[x] = 1
+    for y in adj[x]:
+        if flags[y] == 1:
+            return False
+        if flags[y] == 0 and not dfs(y):
+            return False
+    flags[x] = 2
+    ret.append(x)
+    return True
+
+for i in range(n):
+    if not flags[i]:
+        dfs(i)
