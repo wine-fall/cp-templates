1160d5792164e0c4e95dfd25d51723d9081d1c0b
wine-fall <wine@mail.bnu.edu.cn>
2024-02-18 13:34:01 +0800

    feat: 更新字符串哈希

--- string/string_hash/string_hash.js
+++ string/string_hash/string_hash.js
@@ -1,22 +1,29 @@
 /**
- * @description 将一段字符串序列化为hash
- * @param {string} str
- * @returns {string}
+ * 根据字符串 s 建造 hash 表，并通过 get 函数快速获取该字符串某一段子字符串的 hash 值
  */
-const stringHash = (str) => {
-    const MOD = 1e9 + 7;
-    const base = 31;
-    const getCode = (char) => {
-        return char.charCodeAt() - 'a'.charCodeAt() + 1;
+class Hash {
+    /**
+     * 
+     * @param {string} s 
+     */
+    constructor(s) {
+        const p = 13331;
+        const MOD = 10000079;
+        this.MOD = MOD;
+        const n = s.length;
+        const hash = new Array(n + 1).fill(0);
+        const base = new Array(n + 1).fill(0);
+        hash[0] = 0;
+        base[0] = 1;
+        for (let i = 1; i <= n; i++) {
+            base[i] = (base[i - 1] * p) % MOD;
+            hash[i] = (hash[i - 1] * p + s.charCodeAt(i - 1) - 96) % MOD;
+        }
+        this.hash = hash;
+        this.base = base;
     }
-    const n = str.length;
-    const prefix = Array(n + 1).fill(0);
-    const mul = Array(n + 1).fill(1);
-    for (let i = 1; i < n + 1; i++) {
-        prefix[i] = (prefix[i - 1] * base + getCode(str[i - 1]) ) % MOD;
-        mul[i] = mul[i - 1] * base % MOD;
-    }
-    const getHash = (l, r) => {
-        return (prefix[l] - prefix[r] * mul[l - r] % MOD + MOD) % MOD;
+    get(i, j) {
+        const {hash, base, MOD} = this;
+        return (((hash[j] - hash[i - 1] * base[j - i + 1]) % MOD) + MOD) % MOD;
     }
 }
